bec79552b38153a5e6f0eedad354de9cc1e05694
Diego Pagini <diegopaginidev@gmail.com>
2022-05-09 08:38:42 +0200

    places

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
+import { Place } from 'src/app/interfaces/place.interface';
 import { environment } from 'src/environments/environment';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +10,29 @@ import { environment } from 'src/environments/environment';
 })
 export class MapComponent implements OnInit {
   map: mapboxgl.Map;
+  places: Place[] = [
+    {
+      id: '1',
+      name: 'Fernando',
+      lng: -75.75512993582937,
+      lat: 45.349977429009954,
+      color: '#dd8fee',
+    },
+    {
+      id: '2',
+      name: 'Amy',
+      lng: -75.75195645527508,
+      lat: 45.351584045823756,
+      color: '#790af0',
+    },
+    {
+      id: '3',
+      name: 'Orlando',
+      lng: -75.75900589557777,
+      lat: 45.34794635758547,
+      color: '#19884b',
+    },
+  ];
 
   constructor() {}
 
---
+++ src/app/interfaces/place.interface.ts
@@ -0,0 +1,7 @@
+export interface Place {
+  id: string;
+  name: string;
+  lng: number;
+  lat: number;
+  color: string;
+}
